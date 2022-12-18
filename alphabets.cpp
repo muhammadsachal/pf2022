1f7a29cbaa83731d7aefaa691df067093662c2ca
muhammadsachal <sachalqamar1435@gmail.com>
2022-12-18 14:48:51 +0500

    old tasks
    files

---
+++ alphabets.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+main(){
+cout<<"          __"<<endl;
+cout<<"         /  \\"<<endl;
+cout<<"        / /\\ \\"<<endl;
+cout<<"       / /  \\ \\"<<endl;
+cout<<"      / /    \\ \\"<<endl;
+cout<<"     / /      \\ \\"<<endl;
+cout<<"    / /________\\ \\"<<endl;
+cout<<"   / ___________\\ \\"<<endl;
+cout<<"  / /            \\ \\"<<endl;
+cout<<" / /              \\ \\"<<endl;
+cout<<"/_/                \\_\\"<<endl<<endl<<endl;;
+
+cout<<"*              *"<<endl;
+cout<<"  *          *"<<endl;
+cout<<"    *      *"<<endl;
+cout<<"      *  *"<<endl;
+cout<<"        *"<<endl;
+cout<<"      *  *"<<endl;
+cout<<"    *      *"<<endl;
+cout<<"  *          *"<<endl;
+cout<<"*              *"<<endl<<endl<<endl;
+
+cout<<"___________________"<<endl;
+cout<<"                  /"<<endl;
+cout<<"                 /"<<endl;
+cout<<"               /"<<endl;
+cout<<"             /"<<endl;
+cout<<"           /"<<endl;
+cout<<"         /"<<endl;
+cout<<"       /"<<endl;
+cout<<"     /"<<endl;
+cout<<"   /"<<endl;
+cout<<" /__________________"<<endl<<endl<<endl;
+
+cout<<"*    **"<<endl;
+cout<<"*   **"<<endl;
+cout<<"*  **"<<endl;
+cout<<"* **"<<endl;
+cout<<"* *"<<endl;
+cout<<"****"<<endl;
+cout<<"*  **"<<endl;
+cout<<"*   **"<<endl;
+cout<<"*    **"<<endl;
+cout<<"*     **"<<endl;
+
+cout<<"  ***********"<<endl;
+cout<<" **   "<<endl;
+cout<<"**  "<<endl;
+cout<<"**   "<<endl;
+cout<<" **********"<<endl;
+cout<<"          **"<<endl;
+cout<<"           **"<<endl;
+cout<<"           **"<<endl;
+cout<<"           **"<<endl;
+cout<<" ***********"<<endl<<endl<<endl;
+
+cout<<"*          *"<<endl;
+cout<<"*          *"<<endl;
+cout<<"*          *"<<endl;
+cout<<"*          *"<<endl;
+cout<<"*          *"<<endl;
+cout<<"*          *"<<endl;
+cout<<"*          *"<<endl;
+cout<<"*          *"<<endl;
+cout<<" *        *"<<endl;
+cout<<"   ******"<<endl;
+
+}
---
+++ cow.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+main(){
+system("Color 27");
+
+cout<<"--------------------------------"<<endl<<endl;
+cout<<"       o   ^__^"<<endl<<endl;
+cout<<"        o  (oo)\\________"<<endl<<endl;
+cout<<"           (__)\\        )\\/\\"<<endl<<endl;
+cout<<"                ||----w |"<<endl<<endl;
+cout<<"                ||     ||"<<endl<<endl;
+}
